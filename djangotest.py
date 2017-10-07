1ac295efec315c78cfe9428db1e93c9d4f8519dd
charlotte <ritter_charlotte@hotmail.com>
2017-10-07 14:44:07 +0200

    My Django Girls app, first commit

---
+++ djangotest.py
@@ -0,0 +1,19 @@
+print('Hello, Django girls!') 
+"""psycoelectro bla bla
+bla bla bla"""
+if 3>2:
+    print('it\'s working')
+else:
+    print('no')
+    
+def yaya(name):
+   if name=='charlotte':
+       print('hello cha')
+   else:
+       print('yo')
+yaya('charlotte')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:         #pour chaque nom dans la liste girls
+    print('hello '+name+' !')
+    
